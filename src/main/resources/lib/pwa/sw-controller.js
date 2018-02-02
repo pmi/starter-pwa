15ea7e438019f3b0fbb0f7a5c592d40cb48fa4ca
Christian Westgaard <cwe@enonic.com>
2018-02-02 15:49:42 +0100

    Fix path so webpack inlines it correctly

--- src/main/resources/lib/pwa/sw-controller.js
+++ src/main/resources/lib/pwa/sw-controller.js
@@ -1,12 +1,12 @@
 var mustache = require('/lib/xp/mustache');
-var view = resolve('sw-template.js');
+var view = resolve('/lib/pwa/sw-template.js');
 var helper = require('/lib/helper');
 
 exports.get = function() {
     var appUrl = helper.getAppUrl();
     var baseUrl = helper.getBaseUrl();
     var postfix = '?source=web_app_manifest';
-    
+
     var preCacheRoot;
     if (appUrl === '/') {
         preCacheRoot = '/' + ',\'' + postfix;
@@ -15,7 +15,7 @@ exports.get = function() {
     } else {
         preCacheRoot = appUrl + '\',\'' + appUrl + '/' + '\',\'' + appUrl + '/' + postfix;
     }
-    
+
     return {
         headers: {
             'Service-Worker-Allowed': appUrl
